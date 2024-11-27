7fd9214cc92d40db8dc2046b921b6bb03792bb6a
Sarita <saritaa7113@gmail.com>
2024-11-27 12:10:50 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+function addTask() {
+    let input = document.getElementById('taskInput');
+    let taskText = input.value.trim();
+
+    if (taskText === '') {
+        alert("Please enter Task");
+        return
+    };
+
+    let taskList = document.getElementById('taskList');
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    span.textContent = taskText;
+    span.onclick = function () {
+        toggleComplete(li);
+    };
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '-';
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.onclick = function () {
+        deleteTask(li);
+    };
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    // Clear input
+    input.value = '';
+}
+
+function toggleComplete(taskElement) {
+    taskElement.classList.toggle('completed');
+}
+
+function deleteTask(taskElement) {
+    taskElement.remove();
+}
+
+function clearCompleted() {
+    let completedTasks = document.querySelectorAll('.completed');
+    completedTasks.forEach(task => task.remove());
+}
